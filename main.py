00d6d8f95583cb60be2321560577fc820ead14c6
DEVENDHRAN Ravikumar (INTERN) <RDN11@dsone.3ds.com>
2026-07-02 17:00:24 +0530

    Updated

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return "hello world"
+@app.get("/blog")
+def index(limit : int = 100,published : bool = True , sort : Optional[str] = "a;lksdj"):
+    if(published):
+        return {"data":f"{limit} Published  from the db"}
+    else:
+        return {"data":f"{limit} blogs from the db"}
+
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return "All unpublished blogs"
+
+
+
+@app.get("/blog/{id}")
+def show(id:int):
+    return {"data":id}
+
+
+
+@app.get("/blog/{id}/comments")
+def comments(id:int):
+    return {"data":{1,2,3,4}}
+
+
+class Blog(BaseModel):
+    title : str
+    body : str
+    published : Optional[bool]
+    
+@app.post("/blog")
+def create_blog(blog : Blog):
+    return {"Data":f"Blog is created with {blog.title}"}
