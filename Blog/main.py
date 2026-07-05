dd4bfffe7a23011539f678bb114fdcfde41b2ff8
ravikumar.d2023aiml@sece.ac.in <ravikumar.d2023aiml@sece.ac.in>
2026-07-05 22:21:51 +0530

    need to do more

--- Blog/main.py
+++ Blog/main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
-from .import schema
+from fastapi import FastAPI,Depends,status,Response
+from .import schema,models
+from .database import engine,SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
 @app.post("/blog")
-def create_blog(request : schema.Blog):
-    return request
+def create_blog(request : schema.Blog,db : Session = Depends(get_db),status_code=status.HTTP_201_CREATED):
+    new_blog = models.Blog(title = request.title , body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get("/blog")
+def all(db : Session = Depends(get_db)):
+    blogs =db.query(models.Blog).all()
+    return blogs
+
+@app.get("/blog/{id}")
+def show(id : int,db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
+
